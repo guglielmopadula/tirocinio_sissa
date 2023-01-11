bd47c943a3428061f6e2596b7475471f05308d90
cyberguli <cyberguli@cli-10-110-4-162.WIFIeduroamSTUD.units.it>
2023-01-12 00:29:14 +0100

    disabling cuda because cvxpy still does not support it and it throws errors

--- DeepLearning/surface_nets/navalhull/training.py
+++ DeepLearning/surface_nets/navalhull/training.py
@@ -17,8 +17,6 @@ from models.BEGAN import BEGAN
 import torch
 import numpy as np
 from pytorch_lightning import Trainer
-use_cuda=True if torch.cuda.is_available() else False
-AVAIL_GPUS=torch.cuda.device_count() if torch.cuda.is_available() else 0
 
 NUM_WORKERS = int(os.cpu_count() / 2)
 
@@ -53,14 +51,7 @@ d={
 for wrapper, name in d.items():
     torch.manual_seed(100)
     np.random.seed(100)
-    if use_cuda:
-        trainer = Trainer(accelerator='gpu', devices=AVAIL_GPUS,max_epochs=MAX_EPOCHS,log_every_n_steps=1,track_grad_norm=2,
-                              gradient_clip_val=0.1
-                              )
-    else:
-        trainer=Trainer(max_epochs=MAX_EPOCHS,log_every_n_steps=1,track_grad_norm=2,
-                            gradient_clip_val=0.1
-                            )   
+    trainer=Trainer(max_epochs=MAX_EPOCHS,log_every_n_steps=1,track_grad_norm=2,gradient_clip_val=0.1)   
 
     model=wrapper(data.get_reduced_size(),data.temp_zero,data.local_indices_1,data.local_indices_2,data.newtriangles_zero,data.pca_1,data.pca_2,data.edge_matrix,data.vertices_face,data.cvxpylayer,k=SMOOTHING_DEGREE,latent_dim_1=LATENT_DIM_1,latent_dim_2=LATENT_DIM_2,batch_size=BATCH_SIZE,drop_prob=DROP_PROB)
     print("Training of "+name+ "has started")
