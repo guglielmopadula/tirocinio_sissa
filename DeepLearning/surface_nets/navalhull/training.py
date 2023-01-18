ca2393b5bf4deab72669f3004441989828ddc4cd
cyberguli <guglielmo.padula@aruba.it>
2023-01-18 13:31:38 +0100

    update training to not use slurm

--- DeepLearning/surface_nets/navalhull/training.py
+++ DeepLearning/surface_nets/navalhull/training.py
@@ -16,6 +16,23 @@ import torch
 import numpy as np
 from pytorch_lightning import Trainer
 
+from pytorch_lightning.plugins.environments import SLURMEnvironment
+
+class DisabledSLURMEnvironment(SLURMEnvironment):
+    def detect() -> bool:
+        return False
+
+    @staticmethod
+    def _validate_srun_used() -> None:
+        return
+
+    @staticmethod
+    def _validate_srun_variables() -> None:
+        return
+
+
+
+
 NUM_WORKERS = os.cpu_count()//2
 use_cuda=True if torch.cuda.is_available() else False
 AVAIL_GPUS=torch.cuda.device_count() if torch.cuda.is_available() else 0
@@ -51,11 +68,11 @@ if __name__ == "__main__":
         np.random.seed(100)
         if use_cuda:
             trainer = Trainer(accelerator='gpu', devices=AVAIL_GPUS,max_epochs=MAX_EPOCHS,log_every_n_steps=1,track_grad_norm=2,
-                                  gradient_clip_val=0.1
+                                  gradient_clip_val=0.1, plugins=[DisabledSLURMEnvironment(auto_requeue=False)],
                                   )
         else:
             trainer=Trainer(max_epochs=MAX_EPOCHS,log_every_n_steps=1,track_grad_norm=2,
-                                gradient_clip_val=0.1
+                                gradient_clip_val=0.1,plugins=[DisabledSLURMEnvironment(auto_requeue=False)],
                                 )   
         model=wrapper(data.get_reduced_size(),data.temp_zero,data.local_indices_1,data.local_indices_2,data.newtriangles_zero,data.pca_1,data.pca_2,data.edge_matrix,data.vertices_face,data.cvxpylayer,k=SMOOTHING_DEGREE,latent_dim_1=LATENT_DIM_1,latent_dim_2=LATENT_DIM_2,batch_size=BATCH_SIZE,drop_prob=DROP_PROB)
         print("Training of "+name+ "has started")
