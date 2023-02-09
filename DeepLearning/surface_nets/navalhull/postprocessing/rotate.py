cec8975cbc5e756ef7409849b24fca82b31548e0
cyberguli <cyberguli@cli-10-110-4-162.WIFIeduroamSTUD.units.it>
2023-02-10 00:26:52 +0100

    update rotations

--- DeepLearning/surface_nets/navalhull/postprocessing/rotate.py
+++ DeepLearning/surface_nets/navalhull/postprocessing/rotate.py
@@ -5,12 +5,26 @@ Created on Mon Jan  9 22:36:27 2023
 
 @author: cyberguli
 """
-
+import numpy as np
 import pickle
 import meshio
+from sklearn.linear_model import LinearRegression
+import os
 filename = 'rotate_model.sav'
+
+if not os.path.isfile(filename):
+    m1=meshio.read("../data_objects/hullmoved.stl")
+    m2=meshio.read("../data_objects/hullhalved.stl")
+    linreg=LinearRegression()
+    linreg.fit(m1.points.reshape(-1,3),m2.points.reshape(-1,3))
+    print(linreg.score(m1.points.reshape(-1,3),m2.points.reshape(-1,3)))
+    pickle.dump(linreg, open(filename, 'wb'))
+
+
+
+
+
 linreg = pickle.load(open(filename, 'rb'))
-#pickle.dump(linreg, open(filename, 'wb'))
 for i in range(100):
     #mesh=meshio.read("../inference_objects/AE_{}.stl".format(i))
     #mesh.points=linreg.predict(mesh.points)
